d656bad2957ed2ad3851339a02612aa947e9a117
Hiroki Sato <hrs@allbsd.org>
2016-12-30 22:39:10 +0900

    Add .LZMA and .lzma compression support.
    (cherry picked from commit 8fb156c04b504afc5e200bcb8bf8e0b7e3dd33be)

--- filename.c
+++ filename.c
@@ -817,6 +817,9 @@ easy_lessopen_for_compressedfile(filename)
 	else if (strcmp(".xz", &filename[length - 3]) == 0 ||
 		 strcmp(".XZ", &filename[length - 3]) == 0)
 		return ("| xz -cd %s");
+	else if (strcmp(".lzma", &filename[length - 5]) == 0 ||
+		 strcmp(".LZMA", &filename[length - 5]) == 0)
+		return ("| lzma -cd %s");
 	else
 		return (NULL);
 }
